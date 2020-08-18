a71bd287fc21f6e26b4da411de16f68a8867e717
Bernard Kolobara <me@kolobara.com>
2020-08-18 18:21:19 +0200

    Add async_wormhole future creation benchmark

--- benches/async_benchmark.rs
+++ benches/async_benchmark.rs
@@ -3,6 +3,14 @@ use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 use async_wormhole::AsyncWormhole;
 
 fn async_bench(c: &mut Criterion) {
+    c.bench_function("async_wormhole creation", |b| {
+        b.iter(|| {
+            AsyncWormhole::new(|mut yielder| {
+                yielder.async_suspend(async { 42 } );
+            }).unwrap()
+        })
+    });
+
     c.bench_function("async switch", |b| {
         b.iter_batched(
             || {
