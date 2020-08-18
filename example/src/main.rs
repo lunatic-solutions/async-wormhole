88c1fc1c52c3aa48ab0f5d5f0667db475bcf20a4
Bernard Kolobara <me@kolobara.com>
2020-08-18 19:23:22 +0200

    Fix example

--- example/src/main.rs
+++ example/src/main.rs
@@ -2,9 +2,7 @@ use async_wormhole::AsyncWormhole;
 fn main() {
     let task = AsyncWormhole::new(|mut yielder| {
         let x = yielder.async_suspend(async { 5 } );
-        let m = 345;
         assert_eq!(x, 5);
-        // panic!("What");
         let y = yielder.async_suspend(async { true } );
         assert_eq!(y, true);
         42
